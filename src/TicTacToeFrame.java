975d06a3227ec6292b6ccd87943479f688759b56
coffe <coffe@DESKTOP-HKEHOEQ>
2023-02-28 08:11:42 -0500

    Initial commit

---
+++ src/TicTacToeFrame.java
@@ -0,0 +1,36 @@
+
+import javax.swing.JButton;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wulft
+ */
+public class TicTacToeFrame extends JButton
+{
+    private int row;
+    private int col;
+
+    public TicTacToeFrame(int row, int col) {
+        super();
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+
+
+
+}
